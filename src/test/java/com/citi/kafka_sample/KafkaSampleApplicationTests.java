b737cd9de7d489ad4fcc8cbdef30d9f7be326496
Vikkram <codevikky@gmail.com>
2024-10-24 00:52:11 +0800

    first

---
+++ src/test/java/com/citi/kafka_sample/KafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.kafka_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
