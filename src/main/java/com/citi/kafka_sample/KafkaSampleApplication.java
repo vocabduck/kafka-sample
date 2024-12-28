4f9dbf81dd3b9f939dddd3c1cf21ad096bad1df5
Vikkram <codevikky@gmail.com>
2024-12-28 18:13:37 +0800

    read

--- src/main/java/com/citi/kafka_sample/KafkaSampleApplication.java
+++ src/main/java/com/citi/kafka_sample/KafkaSampleApplication.java
@@ -10,4 +10,30 @@ public class KafkaSampleApplication {
 		SpringApplication.run(KafkaSampleApplication.class, args);
 	}
 
+	@Override
+	public SMCGoldDto read() throws Exception {
+		// Initialize and move to the "results" array
+		if (!initialized) {
+			while (parser.nextToken() != JsonToken.END_OBJECT) {
+				if (Objects.equals(parser.getCurrentName(), "results")) {
+					parser.nextToken(); // Move to START_ARRAY
+					initialized = true;
+					break;
+				}
+			}
+		}
+
+		// Read the next object in the array
+		if (parser.nextToken() == JsonToken.START_OBJECT) {
+			return objectMapper.readValue(parser, SMCGoldDto.class);
+		}
+
+		// Close the parser when the array ends
+		if (parser.currentToken() == JsonToken.END_ARRAY) {
+			parser.close();
+		}
+
+		return null; // No more data to read
+	}
+
 }
